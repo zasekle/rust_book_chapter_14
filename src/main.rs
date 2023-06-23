490f63a5cefbd13b8f394558da825479c1a60520
zasekle <quickshiftx@gmail.com>
2023-06-23 08:59:13 -0400

    Rust book section 14.1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, worldz!");
+    println!("running program!");
+    customizing_builds_with_release_profiles();
+}
+
+fn customizing_builds_with_release_profiles() {
+    //Cargo has two main profiles: the `dev` profile which is run by default when running cargo
+    // build and the `release` profile when cargo build --release is run.
+
+    //Look in Cargo.toml for some more info on changing the profiles.
 }
